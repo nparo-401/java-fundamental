8c6426933d534c8d097e48d2c987c39a50f58bb0
paronicholas <31823783+paronicholas@users.noreply.github.com>
2019-08-07 16:16:39 -0700

    Update Library.java

--- basiclibrary/src/main/java/basiclibrary/Library.java
+++ basiclibrary/src/main/java/basiclibrary/Library.java
@@ -15,7 +15,7 @@ public class Library {
     public static boolean containsDuplicates(int[] arr) {
         if (arr.length == 0) return false;
 
-        for (int i= 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
                     return true;

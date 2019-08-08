2e166c5ca61b58d877e7e63e52fbbef0c0d9b46f
Nicholas Paro <nichparo@amazon.com>
2019-08-08 11:49:06 -0700

    finished basic library methods

--- basiclibrary/src/test/java/basiclibrary/Lab03Test.java
+++ basiclibrary/src/test/java/basiclibrary/Lab03Test.java
@@ -1,7 +1,116 @@
 package basiclibrary;
 
+import org.junit.Test;
+
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class Lab03Test {
+  @Test
+  public void testWeatherData_FullMonth() {
+    int[][] weeklyMonthTemperatures = {
+        {66, 64, 58, 65, 71, 57, 60},
+        {57, 65, 65, 70, 72, 65, 51},
+        {55, 54, 60, 53, 59, 57, 61},
+        {65, 56, 55, 52, 55, 62, 57}
+    };
 
+    assertEquals(
+        "FullMonth : produced data is not outputted correctly",
+        "High: 72\n" +
+            "Low: 51\n" +
+            "Never saw temperature: 63\n" +
+            "Never saw temperature: 67\n" +
+            "Never saw temperature: 68\n" +
+            "Never saw temperature: 69\n",
+        Lab03.weatherData(weeklyMonthTemperatures));
+  }
+
+  @Test
+  public void testWeatherData_EmptyMonth() {
+    int[][] weeklyMonthTemperatures = {{}, {}, {}, {}};
+
+    assertEquals(
+        "EmptyMonth : should return '0'",
+        "0",
+        Lab03.weatherData(weeklyMonthTemperatures)
+    );
+  }
+
+  @Test
+  public void testWeatherData_OneEmptyWeek() {
+    int[][] weeklyMonthTemperatures = {
+        {},
+        {57, 65, 65, 70, 72, 65, 51},
+        {55, 54, 60, 53, 59, 57, 61},
+        {65, 56, 55, 52, 55, 62, 57}
+    };
+
+    assertEquals(
+        "OneEmptyWeek : incorrect values",
+        "High: 72\n" +
+            "Low: 51\n" +
+            "Never saw temperature: 58\n" +
+            "Never saw temperature: 63\n" +
+            "Never saw temperature: 64\n" +
+            "Never saw temperature: 66\n" +
+            "Never saw temperature: 67\n" +
+            "Never saw temperature: 68\n" +
+            "Never saw temperature: 69\n" +
+            "Never saw temperature: 71\n",
+        Lab03.weatherData(weeklyMonthTemperatures)
+    );
+  }
+
+  @Test
+  public void testTally_OneWinner() {
+    List<String> votes = new ArrayList<>();
+    votes.add("Bush");
+    votes.add("Bush");
+    votes.add("Bush");
+    votes.add("Shrub");
+    votes.add("Hedge");
+    votes.add("Shrub");
+    votes.add("Bush");
+    votes.add("Hedge");
+    votes.add("Bush");
+
+    assertEquals(
+        "OneWinner should return 'Bush'",
+        "Bush received the most votes!",
+        Lab03.tally(votes)
+    );
+  }
+
+  @Test
+  public void testTally_NoVotes() {
+    List<String> votes = new ArrayList<>();
+
+    assertEquals(
+        "testTally should return 'Bush'",
+        "0",
+        Lab03.tally(votes)
+    );
+  }
+
+  @Test
+  public void testTally_TieForWinner() {
+    List<String> votes = new ArrayList<>();
+    votes.add("Bush");
+    votes.add("Bush");
+    votes.add("Bush");
+    votes.add("Shrub");
+    votes.add("Shrub");
+    votes.add("Shrub");
+    votes.add("Bush");
+    votes.add("Hedge");
+    votes.add("Shrub");
+
+    assertEquals(
+        "TieForWinner should return 'Bush'",
+        "Tie for the winner, a re-vote is needed",
+        Lab03.tally(votes)
+    );
+  }
 }
--- lab-readme/lab03-readme.md
+++ lab-readme/lab03-readme.md
@@ -1,5 +1,11 @@
 ## Lab 03 - Maps, File I/O, and Testing
 #### Resources:
+* Jack Kinne
+* Travis Cox
+* Brandon Hurrington
+* [HashSet](https://www.baeldung.com/java-hashset)
+* [Collections.frequency](https://www.geeksforgeeks.org/java-util-collections-frequency-java-examples/)
+
 
 #### Files:
 * basiclibrary
@@ -16,9 +22,25 @@
 #### Methods:
 **Basic Library**
 * weatherData(int[][] weatherArr)
-  * 
-* tally(ArrayList<String> votes)
-  * 
+  * takes in an array of integer arrays
+  * Assigns all values to a HashSet and eliminates all duplicate values
+  * Assigns the min and max values using `Collections.min` and `Collections.max`
+  * Begins building the response string with `StringBuilder`
+  * uses a for loop to iterate from the min to 1 less than the max value
+    * if the iterated value is not contained within the HashSet add it to the response string
+  * return the response string
+* tally(List<String> votes)
+  * takes in a string List of votes
+  * Assigns all unique values to a HashSet
+  * Initialize `numVotes = 0` and a response string
+  * Use an extended for loop on the HashSet to get total number of votes for each
+    * Assign a count variable using `Collections.frequency`
+    * If `numVotes < count`
+      * reassign numVotes to count
+      * set response string with the item in the lead
+    * If `numVotes == count`
+      * set response string to the tie response
+  * return the response string
 
 **Linter**
 * readFile()
